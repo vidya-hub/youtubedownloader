4fdec6f1040aad8ee2d0170556ba47b7b37df8a4
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-04-29 16:40:09 +0530

    A lot changed

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
+import pyfiglet
+from tqdm import tqdm
+# ascii_banner = pyfiglet.figlet_format("Youtube Url => MP3")
+# print(ascii_banner)
 import subprocess
-
+import re
+import requests
+print(
+"""
+  __   __          _         _            _   _      _   _____       __  __      _____ 
+  \ \ / /__  _   _| |_ _   _| |__   ___  | | | |_ __| | |_   _|__   |  \/  |_ __|___ / 
+   \ V / _ \| | | | __| | | | '_ \ / _ \ | | | | '__| |   | |/ _ \  | |\/| | '_ \ |_ \ 
+    | | (_) | |_| | |_| |_| | |_) |  __/ | |_| | |  | |   | | (_) | | |  | | |_) |__) |
+    |_|\___/ \__,_|\__|\__,_|_.__/ \___|  \___/|_|  |_|   |_|\___/  |_|  |_| .__/____/ 
+                                                                           |_|         
+ 
+"""
+)
 inputUrl=input("    Enter Video Url:   ")
 command_url = f"youtube-dl -g {inputUrl}"
 command_title = f"youtube-dl -e {inputUrl}"
 titile = (subprocess.check_output(
     command_title.split()).decode('utf-8')).split("\n")
 url = (subprocess.check_output(command_url.split()).decode('utf-8')).split("\n")
-subprocess.check_output(['wget', '-O', f"{titile[0]}.mp3", url[1]])
+_response = requests.get(url[1]).content
+file_size_request = requests.get(url[1], stream=True)
+file_size = int(file_size_request.headers['Content-Length'])
+print(file_size)
+block_size = 1024 
+filename = f"{titile[0]}.mp3"
+t=tqdm(total=file_size, unit='B', unit_scale=True, desc=str(filename), ascii=True)
+with open(filename , 'wb') as f:
+    for data in file_size_request.iter_content(block_size):
+        t.update(len(data))
+        f.write(data)
+t.close()
+print("Audio downloaded successfully")
+# subprocess.check_output(['wget', '-O', f"{titile[0]}.mp3", url[1]])
