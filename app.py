a941ac934380761db5117c0d31c7f894b844e40a
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-04-29 16:46:00 +0530

    little

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-# import pyfiglet
+import pyfiglet
 from tqdm import tqdm
-# ascii_banner = pyfiglet.figlet_format("Youtube Url => MP3")
-# print(ascii_banner)
+ascii_banner = pyfiglet.figlet_format("Youtube Url => MP3")
+print(ascii_banner)
 import subprocess
 # import re
 import requests
-print(
-"""
-  __   __          _         _            _   _      _   _____       __  __      _____ 
-  \ \ / /__  _   _| |_ _   _| |__   ___  | | | |_ __| | |_   _|__   |  \/  |_ __|___ / 
-   \ V / _ \| | | | __| | | | '_ \ / _ \ | | | | '__| |   | |/ _ \  | |\/| | '_ \ |_ \ 
-    | | (_) | |_| | |_| |_| | |_) |  __/ | |_| | |  | |   | | (_) | | |  | | |_) |__) |
-    |_|\___/ \__,_|\__|\__,_|_.__/ \___|  \___/|_|  |_|   |_|\___/  |_|  |_| .__/____/ 
-                                                                           |_|         
+# print(
+# """
+#   __   __          _         _            _   _      _   _____       __  __      _____ 
+#   \ \ / /__  _   _| |_ _   _| |__   ___  | | | |_ __| | |_   _|__   |  \/  |_ __|___ / 
+#    \ V / _ \| | | | __| | | | '_ \ / _ \ | | | | '__| |   | |/ _ \  | |\/| | '_ \ |_ \ 
+#     | | (_) | |_| | |_| |_| | |_) |  __/ | |_| | |  | |   | | (_) | | |  | | |_) |__) |
+#     |_|\___/ \__,_|\__|\__,_|_.__/ \___|  \___/|_|  |_|   |_|\___/  |_|  |_| .__/____/ 
+#                                                                            |_|         
  
-"""
-)
+# """
+# )
 inputUrl=input("    Enter Video Url:   ")
 command_url = f"youtube-dl -g {inputUrl}"
 command_title = f"youtube-dl -e {inputUrl}"
