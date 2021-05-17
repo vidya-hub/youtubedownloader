7e7937e253057f69f8f34ff580f1bbe05732e2db
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-05-17 17:37:28 +0530

    lot Changed

--- youtubeDownload.py
+++ youtubeDownload.py
@@ -1,33 +1,37 @@
-
-from tqdm import tqdm
-
 import subprocess
 
 print(" Youtube Downloader ")
 
 inputUrl = input("    Enter Video Url:   ")
 command_title = f"youtube-dl -e {inputUrl}"
-command_format=f"youtube-dl -F {inputUrl}"
-
-
-selected_val=int(input("""
-    Select the Option
-    1 -- Download Video With Different Formats
-    2 -- Download Audio Only                       
-"""))
-titile = (subprocess.check_output(
+command_format = f"youtube-dl -F {inputUrl}"
+title = (subprocess.check_output(
     command_title.split()).decode('utf-8')).split("\n")
-if selected_val==1:
-    print(subprocess.check_output(command_format.split()).decode('utf-8'))
-    
-    selected_format=int(input("""
-        Enter The Selected Formated Code         
-        """))
-    command=f"youtube-dl -f {selected_format} {inputUrl}"
+print(title[0])
+selected_val = int(input("""
+
+    1 -- Download Video With Different Formats
+    2 -- Download Audio Only       
+                    
+    Select the Option :   """))
+
+if selected_val == 1:
+    res = (subprocess.check_output(
+        command_format.split()).decode('utf-8').split("\n"))
+    for data in res[3:]:
+        try:
+            print(data.split()[0] + " ==> " +
+                data.split()[2]+" "+data.split()[3] +" -- "+data.split()[len(data.split())-1])
+        except:
+            print("")
+    selected_format = int(input("""
+    Enter The Selected Formated Code         
+    """))
+    command = f"youtube-dl -f {selected_format} {inputUrl}"
     print(command)
+    # try:
     print(subprocess.run(command.split()))
-    
-elif selected_val==2:
+
+elif selected_val == 2:
     print("Downloading Audio......")
     print(subprocess.run(f"youtube-dl -f bestaudio {inputUrl}".split()))
-    
