40c22e1667163ac71f9d215e69e343d9afd571e1
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-05-17 13:37:12 +0530

    Little Changes

--- youtubeDownload.py
+++ youtubeDownload.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 
 import subprocess
 
-print(" Youtube Url To MP3 Converter ")
+print(" Youtube Downloader ")
 
 inputUrl = input("    Enter Video Url:   ")
 command_title = f"youtube-dl -e {inputUrl}"
@@ -11,12 +11,9 @@ command_format=f"youtube-dl -F {inputUrl}"
 
 
 selected_val=int(input("""
-                       
-                       Select the Option
-                       
-                       1 -- Download Video With Different Formats
-                       2 -- Download Audio Only                       
-                       
+    Select the Option
+    1 -- Download Video With Different Formats
+    2 -- Download Audio Only                       
                        """))
 titile = (subprocess.check_output(
     command_title.split()).decode('utf-8')).split("\n")
@@ -24,9 +21,7 @@ if selected_val==1:
     print(subprocess.check_output(command_format.split()).decode('utf-8'))
     
     selected_format=int(input("""
-        
-        Enter The Selected Formated Code 
-        
+        Enter The Selected Formated Code         
         """))
     command=f"youtube-dl -f {selected_format} {inputUrl}"
     print(command)
