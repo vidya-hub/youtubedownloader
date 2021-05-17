68aa83356fa38f8a45bc6f3b1cd8eaa214a38a3c
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-05-17 13:31:29 +0530

    Complete Youtube Downloader

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 # import pyfiglet
-from tqdm import tqdm
-# ascii_banner = pyfiglet.figlet_format("Youtube Url => MP3")
-# print(ascii_banner)
-import subprocess
-print(" Youtube Url To MP3 Converter ")
-# youtube to mp3
-# import re
 import requests
+from tqdm import tqdm
+import subprocess
+from clint.textui import progress
+print(" Youtube Url To MP3 Converter ")
 # print(
 # """
-#   __   __          _         _            _   _      _   _____       __  __      _____ 
-#   \ \ / /__  _   _| |_ _   _| |__   ___  | | | |_ __| | |_   _|__   |  \/  |_ __|___ / 
-#    \ V / _ \| | | | __| | | | '_ \ / _ \ | | | | '__| |   | |/ _ \  | |\/| | '_ \ |_ \ 
+#   __   __          _         _            _   _      _   _____       __  __      _____
+#   \ \ / /__  _   _| |_ _   _| |__   ___  | | | |_ __| | |_   _|__   |  \/  |_ __|___ /
+#    \ V / _ \| | | | __| | | | '_ \ / _ \ | | | | '__| |   | |/ _ \  | |\/| | '_ \ |_ \
 #     | | (_) | |_| | |_| |_| | |_) |  __/ | |_| | |  | |   | | (_) | | |  | | |_) |__) |
-#     |_|\___/ \__,_|\__|\__,_|_.__/ \___|  \___/|_|  |_|   |_|\___/  |_|  |_| .__/____/ 
-#                                                                            |_|         
- 
+#     |_|\___/ \__,_|\__|\__,_|_.__/ \___|  \___/|_|  |_|   |_|\___/  |_|  |_| .__/____/
+#                                                                            |_|
+
 # """
 # )
-inputUrl=input("    Enter Video Url:   ")
+
+
+inputUrl = input("    Enter Video Url:   ")
 command_url = f"youtube-dl -g {inputUrl}"
 command_title = f"youtube-dl -e {inputUrl}"
+# command_title = f"youtube-dl -e {inputUrl}"
 titile = (subprocess.check_output(
     command_title.split()).decode('utf-8')).split("\n")
 url = (subprocess.check_output(command_url.split()).decode('utf-8')).split("\n")
-file_size_request = requests.get(url[1], stream=True)
-file_size = int(file_size_request.headers['Content-Length'])
+if len(url) == 2:
+    file_size_request = requests.get(url[1], stream=True)
+    file_size = int(file_size_request.headers['Content-Length'])
+    subprocess.check_output(['wget', '-O', f"{titile[0]}.mp3", url[1]])
+else:
+    print("Invalid Url Please Enter Correct Url")
+
 # print(file_size)
-# block_size = 1024 
+# block_size = 1024
 # filename = f"{titile[0]}.mp3"
 # t=tqdm(total=file_size, unit='A', unit_scale=True, desc=str(filename))
 # with open(filename , 'wb') as f:
@@ -36,4 +41,3 @@ file_size = int(file_size_request.headers['Content-Length'])
 #         f.write(data)
 # t.close()
 # print("Audio downloaded successfully")
-subprocess.check_output(['wget', '-O', f"{titile[0]}.mp3", url[1]])
---
+++ youtubeDownload.py
@@ -0,0 +1,38 @@
+
+from tqdm import tqdm
+
+import subprocess
+
+print(" Youtube Url To MP3 Converter ")
+
+inputUrl = input("    Enter Video Url:   ")
+command_title = f"youtube-dl -e {inputUrl}"
+command_format=f"youtube-dl -F {inputUrl}"
+
+
+selected_val=int(input("""
+                       
+                       Select the Option
+                       
+                       1 -- Download Video With Different Formats
+                       2 -- Download Audio Only                       
+                       
+                       """))
+titile = (subprocess.check_output(
+    command_title.split()).decode('utf-8')).split("\n")
+if selected_val==1:
+    print(subprocess.check_output(command_format.split()).decode('utf-8'))
+    
+    selected_format=int(input("""
+        
+        Enter The Selected Formated Code 
+        
+        """))
+    command=f"youtube-dl -f {selected_format} {inputUrl}"
+    print(command)
+    print(subprocess.run(command.split()))
+    
+elif selected_val==2:
+    print("Downloading Audio......")
+    print(subprocess.run(f"youtube-dl -f bestaudio {inputUrl}".split()))
+    
